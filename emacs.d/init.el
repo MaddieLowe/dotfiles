cfe2471413e9937d3bcc542af3404e5f8bf01a90
Darryl Bidulock <darryl@arterys.com>
2017-02-27 13:32:56 -0700

    Remove the auto filled in content

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,6 @@
+; Setup package repositories, install use-package if needed, and pull in
+; everyting else from the settings.org file
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -16,18 +19,4 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-mode t)
- '(package-selected-packages
-   (quote
-    (flycheck web-mode use-package rainbow-mode rainbow-delimiters powerline multiple-cursors magit less-css-mode helm-git helm-ag find-file-in-repository dockerfile-mode docker color-theme-sanityinc-tomorrow auto-complete ag ac-js2))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
